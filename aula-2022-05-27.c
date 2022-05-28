03afafeaf7e492504ef6977f78b2111b31d72c74
EzequielTeodoro <106414490+EzequielTeodoro@users.noreply.github.com>
2022-05-27 22:14:24 -0300

    Add files via upload

---
+++ aula-2022-05-27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+printf ("oi mundo \n");
+printf ("%10d \n",27);
+printf ("%05.2f \n", 7.14);
+printf ("%05.2f \n", 27.2);
+printf ("%30s \n", "fluminense");
+printf ("Nome: %s Idade: %d \n",
+         "Ezequiel", 21);
+}
+
+

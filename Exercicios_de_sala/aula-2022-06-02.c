e6a7fe74c0a69d6a8c1b0545734e0dab9bacbd72
EzequielTeodoro <106414490+EzequielTeodoro@users.noreply.github.com>
2022-06-02 19:51:13 -0300

    Update aula-2022-06-02.c

--- Exercicios_de_sala/aula-2022-06-02.c
+++ Exercicios_de_sala/aula-2022-06-02.c
@@ -1,1 +1,37 @@
 
+/*
+ler as variaveis reais com scanf para cada dia 
+
+#include <stdio.h>
+int main()
+{
+matricula: 2018104179
+
+float segunda, terca, quarta, quinta, sexta, soma, pagaseg, pagater, pagaqua, pagaqui, pagasex, saldopos, ;
+printf ("Informe o valor recebido na segunda: \n");
+scanf ("%f", & segunda);
+printf ("Informe o valor da despesa na segunda: \n");
+scanf ("%f", & pagaseg);
+printf ("Informe o valor recebido na terca: \n");
+scanf ("%f", & terca);
+printf ("Informe o valor da despesa na terca: \n");
+scanf ("%f", & pagater);
+printf ("Informe o valor recebido na quarta: \n");
+scanf ("%f", & quarta);
+printf ("Informe o valor da despesa na quarta: \n");
+scanf ("%f", & pagaqua);
+printf ("Informe o valor recebido na quinta: \n");
+scanf ("%f", & quinta);
+printf ("Informe o valor da despesa na quinta: \n");
+scanf ("%f", & pagaqui);
+printf ("Informe o valor recebido na sexta: \n");
+scanf ("%f", & sexta);
+printf ("Informe o valor da despesa na sexta: \n");
+scanf ("%f", & pagsex)
+printf ("Informe o valor recebido na semana: \n");
+scanf ("%f", & semana);
+printf ("Informe o valor despesa na paga: \n");
+scanf ("%f", & paga);
+
+soma= segunda + terca + quarta + quinta + sexta;
+saldo= pagaseg + pagater + pagaqua + pagaqui + pagsex;

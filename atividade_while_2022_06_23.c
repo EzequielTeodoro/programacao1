b1aae432e2890a7e435afbd0d5f78b1a9be13680
EzequielTeodoro <106414490+EzequielTeodoro@users.noreply.github.com>
2022-06-23 19:02:48 -0300

    Create atividade_while_2022_06_23.c

---
+++ atividade_while_2022_06_23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   int i;
+   printf("Sao divisiveis por 3: ");
+   i= 1;
+   while (i <= 40) 
+   {
+     if (i % 3 == 0)
+       printf ("%d\n", i);
+       i++;
+   }
+ 
+ 
+}

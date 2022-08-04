6ea12e7d88cf113a1d05cdbb86bab4ed764e454d
EzequielTeodoro <106414490+EzequielTeodoro@users.noreply.github.com>
2022-08-04 20:08:45 -0300

    Create ponteiros_funcao-04-08-2022.c

---
+++ Exercicios_de_sala/ponteiros_funcao-04-08-2022.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+      
+float media_notas(int n)
+{
+      float *notas, media;
+      int i;
+      
+      // criando o vetor:
+      notas = malloc(n*sizeof(float));
+      
+      for(i=0; i<n; i++)
+      {
+           printf("Aluno %d:", i);
+           scanf("%f", &notas[i]);
+      }
+      media = 0.0;
+      for (i=0; i<n; i++){
+          media += notas[i];
+      }
+      media = media/n;
+     
+      return media;
+}
+   
+int main(){
+      float m;
+      m = media_notas(3);
+      printf("media: %.2f\n", m);
+}

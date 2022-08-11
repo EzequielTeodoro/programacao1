ec8d743ac4e39b147bda7e5549c67f3a4f8f760d
EzequielTeodoro <106414490+EzequielTeodoro@users.noreply.github.com>
2022-08-11 19:03:29 -0300

    Create aula11-08.c

---
+++ Exercicios_de_sala/aula11-08.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void esc_num(int num){
+     printf("número: %d\n", num);
+     if(num>0)
+        esc_num(num-1);
+       
+}
+ 
+ int main(){
+     esc_num(10);
+    
+}
+ 
+
+
